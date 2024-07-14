4425e1c8407e62142c088a8083e15b43bbd4edf4
ganesh gone <ganeshgone2003@gmail.com>
2024-07-15 00:49:43 +0530

    added user isAuth function

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,5 +1,6 @@
 const {response}= require('express');
 const UserService = require('../services/user-service');
+const { errors } = require('playwright');
 
 const userService= new UserService();
 
@@ -46,7 +47,29 @@ const signIn= async (req,res) => {
     }
 }
 
+const isAuthenticated = async (req, res) => {
+    try {
+        const token = req.headers['x-access-token'];
+        const response = await userService.isAuthenticated(token);
+        return res.status(200).json({
+            success: true,
+            err: {},
+            data: response,
+            message: 'user is authenticated and token is valid'
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: 'Something went wrong',
+            data: {},
+            success: false,
+            err: error
+        });
+    }
+}
+
 module.exports={
     create,
-    signIn
+    signIn,
+    isAuthenticated
 }
